c88c1bce27f5f4d04bc72fe84592e40d39f000ec
Emily Ellison <emilyellison986@gmail.com>
2012-04-29 20:04:31 -0500

    Add all characteristics to profile (- IBU and SRM)

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -19,7 +19,14 @@ class BeersController < ApplicationController
   
   def show
     @beer = Beer.find_by_id(params[:id])
-    @chars = { :sweet => @beer.sweet, :bitter => @beer.bitter, :sour => @beer.sour }
+    @chars = { :abv => @beer.abv, :sweet => @beer.sweet, :bitter => @beer.bitter, :sour => @beer.sour,
+               :chocolate => @beer.chocolate, :nutty => @beer.nutty, :fruity => @beer.fruity,
+               :floral => @beer.floral, :light => @beer.light, :medium => @beer.medium,
+               :dark => @beer.dark, :smooth => @beer.smooth, :hoppy => @beer.hoppy, 
+               :spicy => @beer.spicy, :tart => @beer.tart, :citrus => @beer.citrus,
+               :creamy => @beer.creamy, :vanilla => @beer.vanilla, :acidic => @beer.acidic,
+               :smoky => @beer.smoky, :cloves => @beer.cloves, :crisp => @beer.crisp,
+               :rich => @beer.rich }
   end
   
   def edit
